1debea1cadec19e98149860645de3391fa5fc399
nitram509 <maki@bitkings.de>
2014-10-25 13:08:17 +0200

    checked PHP - Java cross compatibility

--- java/src/main/java/nimbus_jose_jwt/asym/NimbusVerifier.java
+++ java/src/main/java/nimbus_jose_jwt/asym/NimbusVerifier.java
@@ -5,28 +5,36 @@ import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jwt.SignedJWT;
 
+import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
 
 public class NimbusVerifier {
 
   public static void main(String[] args) {
     try {
-    String token = new NimbusProvider().create();
-      new NimbusVerifier().verify(token);
+      String token = new NimbusProvider().create();
+      NimbusVerifier verifier = new NimbusVerifier();
+      verifier.verify(token, ExampleRsaKeyPair.createPublicKey());
+      System.out.println("-------");
+      verifier.verify(tokenFromPhpExampleCode(), RsaPublicKey.createPublicKey());
     } catch (ParseException | JOSEException e) {
       throw new RuntimeException(e);
     }
   }
 
-  public void verify(String token) throws ParseException, JOSEException {
+  public void verify(String token, RSAPublicKey publicKey) throws ParseException, JOSEException {
     SignedJWT signedJWT = SignedJWT.parse(token);
     System.out.println("Used KeyID: " + signedJWT.getHeader().getKeyID());
 
-    JWSVerifier verifier = new RSASSAVerifier(ExampleRsaKeyPair.createPublicKey());
+    JWSVerifier verifier = new RSASSAVerifier(publicKey);
     boolean verified = signedJWT.verify(verifier);
     System.out.println("JWS Signature is valid: " + verified);
 
     System.out.println("JWS payload, subject: " + signedJWT.getJWTClaimsSet().getSubject());
+  }
+
+  private static String tokenFromPhpExampleCode() {
+    return "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImlkLTA4MTUifQ.eyJzdWIiOiJzb21lb25lQGV4YW1wbGUuY29tIiwiZXhwIjoxNDE0MjMzODg4fQ.WHw6fYPcdJsn2qmBF8OEqLm8zABhyWXY3H9a6CM-hWgvgD7PwxSDBahz-0-OrqTeBF_6afD63PTGLMAEGKKbmZbPxMorRqZ9e4tSqyR9v2rRBlqeSuP0kNUySEeDmPskNyc8oh_bcI-xU5XodMsqYqk-H_38cymDclWTEB9tUYI";
   }
 
 }
---
+++ java/src/main/java/nimbus_jose_jwt/asym/RsaPublicKey.java
@@ -0,0 +1,39 @@
+package nimbus_jose_jwt.asym;
+
+import org.jose4j.base64url.SimplePEMEncoder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+class RsaPublicKey {
+
+  static RSAPublicKey createPublicKey() {
+    try {
+      X509EncodedKeySpec spec = new X509EncodedKeySpec(deserializePublicKey());
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      return (RSAPublicKey) kf.generatePublic(spec);
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static byte[] deserializePublicKey() throws IOException {
+    File pemFile = new File("php/src/rsa_publickey.pem");
+    assert pemFile.exists();
+    BufferedReader reader = new BufferedReader(new FileReader(pemFile));
+    StringBuilder sb = new StringBuilder();
+    for (String s; (s = reader.readLine()) != null; ) {
+      if (s.trim().startsWith("-----BEGIN")) continue;
+      if (s.trim().startsWith("-----END")) continue;
+      sb.append(s).append('\n');
+    }
+    return SimplePEMEncoder.decode(sb.toString());
+  }
+}
